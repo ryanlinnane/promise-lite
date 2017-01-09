29b23195f190d76af8d92dc18b37258f8a69a67d
ryanlinnane <magicantler@gmail.com>
2017-01-08 18:41:57 -0600

    remove some comments

--- src/test/main.js
+++ src/test/main.js
@@ -1,142 +1,29 @@
-// import Promise from '../Promise'
-//
-// const P = new Promise((resolve, reject) => {
-//   setTimeout(() => {
-//     Math.random() > .0 ? resolve('yay') : reject('nah')
-//   }, 1000)
-// }).then(function(resolve) {
-//   console.log('resolve callback ', resolve)
-//   //if we return here. it should go back into the promise chain. and it can be async here.
-//   // return new Promise((resolve, reject) => {
-//   //   setTimeout(() => {
-//   //     Math.random() > .5 ? resolve('yay2') : reject('nah2')
-//   //   }, 1000)
-//   // })
-//   return 5
-// }, function(reject) {
-//   console.log('reject callback ', reject)
-//
-// }).then(function(resolve) {
-//   console.log('resolve callback ', resolve)
-// }, function(reject) {
-//   console.log('reject callback ', reject)
-// })
-//
-//
-
-//
-// import Promise from '../Promise';
-// const P = new Promise((res, rej) => {
-//     res(123);
-// });
-// let testsPassed = 0;
-// const P2 = P.then((value) => {
-//     console.log('P resolved.  Value is ' + value);
-//     testsPassed += (value === 123);
-// });
-// const P3 = P.then((value) => {
-//     console.log('P resolved.  Value is ' + value);
-//     testsPassed += (value === 123);
-//     return 5
-// }).then((value2) => {
-//   console.log('running after p3', value2)
-//   return 7
-// }).then((value) => {
-//   return new Promise((resolve, reject) => {
-//     setTimeout(() => {
-//       console.log('fin executing async')
-//       resolve(1000)
-//     }, 1000)
-//     console.log('start executing async')
-//   })
-// });
-//
-// P3.then((last) => {
-//   console.log('last ', last)
-// })
-// setTimeout(() => {
-//     const total = 2;
-//     console.log(`${testsPassed} / ${total} tests passed.`);
-// }, 4000);
-
-
-
-
-//TODO: test 3
-// import Promise from '../Promise'
-// const P = new Promise((resolve, reject) => {
-//   setTimeout(() => {
-//     resolve(1)
-//   }, 2000)
-// }).then(result => {  //this should append to the new promise.
-//   return new Promise((resolve, reject) => {
-//     setTimeout(() => {
-//       resolve(2)
-//     }, 2000)
-//   }).then(result => {
-//     return new Promise((resolve, reject) => {
-//       setTimeout(() => {
-//         resolve(3)
-//       }, 2000)
-//     })
-//   })
-//   //note that the final .then is called once the prior promise chain actually completes.
-// }).then(result => {
-//   console.log('final then ', result)
-//   //if we return a new promise here.... p will get set
-//   return new Promise((resolve, reject) => {
-//     resolve(4)
-//   })
-// })
-//
-// //all the .thens on the same promise reference do get calls in order on it's resolution.
-// //so we do need an array of then callbacks.
-// P.then(result => {
-//   console.log('result ', result)
-//   return new Promise((resolve, reject) => {
-//     resolve(5)
-//   })
-// }).then(console.log)
-// P.then(result => {
-//   console.log('result ', result)
-// })
-
-
-
-
-
-
 import Promise from '../Promise'
 const P = new Promise((resolve, reject) => {
   setTimeout(() => {
     resolve(1)
   }, 2000)
 }).then(result => {  //this should append to the new promise.
-    console.log('result 1 ', result)
+    console.log('first top layer .then ', result)
     return result + 1
-  // return new Promise((resolve, reject) => {
-  //   setTimeout(() => {
-  //     resolve(2)
-  //   }, 2000)
-  // })
   //note that the final .then is called once the prior promise chain actually completes.
 }).then(result => {
-  console.log('final then ', result)
+  console.log('second top layer .then ', result)
   //if we return a new promise here.... p will get set
   return new Promise((resolve, reject) => {
-    resolve(4)
+    resolve(result+1)
   })
-}).then((promise) => {
-  console.log('promise ', promise)
+}).then(result => {
+  console.log('promise ', result)
   return new Promise((resolve, reject) => {
-    // resolve('hi')
     setTimeout(() => {
-      console.log('resolving hi')
-      resolve('hi')
+      console.log('resolving third top layer ' + result)
+      resolve(result+1)
     }, 1000)
   }).then().then(t => {
+    console.log('inner promise ' + t)
     return t
   })
-}).then(d => {
-  console.log('d ',d)
+}).then(result => {
+  console.log('final: ', result)
 })
